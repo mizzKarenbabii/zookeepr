d844c65e8c6a2eaaf23bfabc9518e521a1848098
mizzKarenbabii <mizzkarenbabii@gmail.com>
2022-02-04 22:08:36 -0500

    added filterbyquery

--- server.js
+++ server.js
@@ -2,11 +2,28 @@ const express = require('express');
 const app = express();
 const { animals } = require('./data/animals.json');
 
+function filterByQuery(query, animalsArray) {
+    let filteredResults = animalsArray;
+    if (query.diet){
+        filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
 
+    }
+    if (query.species) {
+        filteredResults = filteredResults.filter (animal => animal.species === query.species);
+    }
+    if (query.name) {
+        filteredResults = filteredResults.filter(animal => animal.name === query.name);
+    }
+    return filteredResults;
+ };
 
 
 app.get('/api/animals', (req, res) => {
-    res.json(animals);
+  let results = animals;
+  if (req.query) {
+      results = filterByQuery(req.query, results);
+  }
+    res.json(results);
 });
 
 app.listen(3001, () => {
